717b72826c64bceb248b948e96c78f828b41c4d0
FRC Team #5115 at Wheaton High School <frc-team5115@users.noreply.github.com>
2017-05-08 16:24:35 -0400

    Finished vision code

--- Vision2017.py
+++ Vision2017.py
@@ -1,12 +1,11 @@
-import math
-import time
 from networktables import NetworkTable
+import time
 import cv2
 import numpy as np
 import os
 
-os.system("uvcdynctrl -s 'Exposure, Auto' 1")
-os.system("uvcdynctrl -s 'Exposure (Absolute)' 5")
+#os.system("uvcdynctrl -s 'Exposure, Auto' 1")
+#os.system("uvcdynctrl -s 'Exposure (Absolute)' 5")
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -17,23 +16,28 @@ NetworkTable.initialize()
 
 nt = NetworkTable.getTable('pi')
 
-boilercam = cv2.VideoCapture(0)
-cam.set(3, 160)
-cam.set(4, 120)
-
 gearcam = cv2.VideoCapture(1)
-cam.set(3, 160)
-cam.set(4, 120)
+gearcam.set(3, 160)
+gearcam.set(4, 120)
+
+boilercam = cv2.VideoCapture(0)
+boilercam.set(3, 160)
+boilercam.set(4, 120)
 
 counter = 0
 
-def getOffsetsGear():
+def getOffsetsGear(default):
 	ret, frame = gearcam.read()
 	height, width, channels = frame.shape
 	hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-	lower = np.array([0, 150, 50])
+	lower = np.array([20, 150, 150])
 	upper = np.array([75, 255, 255])
 	thresh = cv2.inRange(hsv, lower, upper)
+
+	#cv2.imshow('unfiltered', frame)
+	#cv2.imshow('filtered', thresh)
+	cv2.waitKey(1)
+
 	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 	maxarea = 0
@@ -44,42 +48,45 @@ def getOffsetsGear():
 	centery1 = 0
 	centery2 = 0
 	centery = 0
-	contour = 0
+	contour1 = 0
 	
 	pixelOffsetX = 0
 	pixelOffsetY = 0
 	
-	for c in contours:
+	for i in range(len(contours)):
+		c = contours[i]
 		m = cv2.moments(c)
 		if m['m00'] > maxarea:
 			centerx1 = m['m10'] / m['m00']
 			centery1 = m['m01'] / m['m00']
 			maxarea = m['m00']
-			contour = c
+			contourIndex = i
 	
-	for c in contours:
+	for i in range(len(contours)):
+		c = contours[i]
 		m2 = cv2.moments(c)
-		if m2['m00'] > almostmaxarea and m2['m00'] < maxarea:
+		if m2['m00'] > almostmaxarea and i != contourIndex:
 			centerx2 = m2['m10'] / m2['m00']
 			centery2 = m2['m01'] / m2['m00']
-			lastmaxarea = m2['m00']
-			contour = c
+			almostmaxarea = m2['m00']
 	
 	centerx = (centerx1 + centerx2) / 2
 		
 	pixelOffsetX = centerx - (width / 2)
 
-	#print str(pixelOffsetY)
-	#print str(pixelOffsetX)
+	print str(maxarea) + ' ' + str(almostmaxarea) + ' ' + str(pixelOffsetX)
 
-	angleOffsetX = 45 * pixelOffsetX / width
-	return angleOffsetX
+	if maxarea > 40 and almostmaxarea > 40:
+		return pixelOffsetX
+	else:
+		return default
 	
 def getOffsetsBoiler():
+	ret, frame = boilercam.read()
 	height, width, channels = frame.shape
 	hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-	lower = np.array([0, 150, 50])
-	upper = np.array([75, 255, 255])
+	lower = np.array([35, 100, 150])
+	upper = np.array([75, 255, 220])
 	thresh = cv2.inRange(hsv, lower, upper)
 	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -105,25 +112,25 @@ def getOffsetsBoiler():
 	#print str(pixelOffsetY)
 	#print str(pixelOffsetX)
 
-	angleOffsetX = 73 * pixelOffsetX / width
-	return angleOffsetX, pixelOffsetY
+	return pixelOffsetX, pixelOffsetY
 
+lastTime = time.clock()
 while True:
 	x, y = getOffsetsBoiler()
-	gear = getOffsetsGear()
+	gear = getOffsetsGear(1000)
 
-	print str(counter)
+	#print str(counter)
 	counter += 1
 
-	if ((x == 0) and (y == 0)):
-		print("ON TARGET")
-	else:
-		print str(x)
-		print str(y)
-		print str(gear)
+	#print str(x)
+	#print str(y)
+	#print str(gear)
 	if nt.isConnected():
 		nt.putNumber('xOffset', x)
 		nt.putNumber('yOffset', y)
 		nt.putNumber('gearOffset', gear)
-		
-	time.sleep(.1)
+
+	nowTime = time.clock()
+	#print 1 / (nowTime - lastTime)
+	lastTime = time.clock()
+
